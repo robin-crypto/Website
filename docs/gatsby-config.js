656d8832d44372f996bb2e123429c9779286a244
ncoelho <nunovc@gmail.com>
2019-10-30 16:29:03 +0100

    sdfs

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,3 +1,5 @@
+var proxy = require('http-proxy-middleware')
+
 module.exports = {
   siteMetadata: {
     title: "Gatsby Theme Elements - Template Example",
@@ -8,15 +10,24 @@ module.exports = {
 	plugins: [
 	`gatsby-theme-elements`,
 	  {
-	    resolve: `gatsby-plugin-netlify-cms`,
-	    options: {
-	      modulePath: `path/to/custom/script.js`, // default: undefined
-	      enableIdentityWidget: true,
-	      publicPath: `admin`,
-	      htmlTitle: `Content Manager`,
-	      htmlFavicon: `path/to/favicon`,
-	      includeRobots: false,
-	    },
-	  },
-	]
+	    resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
+  ],
+  // for avoiding CORS while developing Netlify Functions locally
+  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+  developMiddleware: app => {
+    app.use(
+      '/.netlify/functions/',
+      proxy({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '/.netlify/functions/': '',
+        },
+      })
+    )
+  },
 }
